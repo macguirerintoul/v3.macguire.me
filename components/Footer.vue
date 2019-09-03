753d4701e4a8da774f7644b726690fea63d7d935
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-09-02 21:14:01 -0700

    add email subscription

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,9 +3,16 @@
     <div class="footer-content">
       <h2>Macguire Rintoul</h2>
       <p>Experience designer & developer</p>
-      <label for="subscribe-input">Subscribe for occasional emails</label>
-      <input id="subscribe-input" type="email" />
-      <button>Subscribe</button>
+      <h3>Like email?</h3>
+      <label class="footer-subscribe-label" for="subscribe-input">Email</label>
+      <input
+        id="subscribe-input"
+        v-model="email"
+        class="footer-subscribe-input"
+        type="email"
+        placeholder="Email"
+      />
+      <button @click="addSubsriber">Subscribe</button>
       <div class="featured-block-container">
         <FeaturedBlock type="randomStar" />
         <FeaturedBlock type="lastCommit" />
@@ -21,6 +28,22 @@ import FeaturedBlock from '~/components/FeaturedBlock'
 export default {
   components: {
     FeaturedBlock,
+  },
+  data() {
+    return {
+      email: '',
+    }
+  },
+  methods: {
+    addSubsriber() {
+      console.log('Attempting to add subscriber...')
+      fetch('https://api.macguire.now.sh/api/add-subscriber', {
+        method: 'post',
+        body: `{email: ${this.email}}`,
+      }).then(response => {
+        console.log(response)
+      })
+    },
   },
 }
 </script>
